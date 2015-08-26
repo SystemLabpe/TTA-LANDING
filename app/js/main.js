86c50fb4ce24718723f9a3da4282c2018238e975
Juan Mosquera <juan.mosquera@systemlab.com.pe>
2015-08-26 01:40:36 -0500

    add sections: home, products, ranges, top team alliance

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,22 @@
 "use strict";
 
 jQuery(document).ready(function($){
-
+  // $(".product-fancybox").fancybox({
+  //   helpers : {
+  //     overlays : {
+  //       css : {
+  //         'background' : 'rgba(58, 42, 45, 0.95)'
+  //       }
+  //     }
+  //   },
+  //   maxWidth  : 800,
+  //   maxHeight : 600,
+  //   fitToView : false,
+  //   width   : '70%',
+  //   height    : '70%',
+  //   autoSize  : false,
+  //   closeClick  : false,
+  //   openEffect  : 'none',
+  //   closeEffect : 'none'
+  // });
 });
